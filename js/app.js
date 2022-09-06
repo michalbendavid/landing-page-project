cfd8a9c7f814c3067a28c93870b93a9f287bc68a
michalbendavid <michalbendavid770@gmail.com>
2022-09-06 14:10:39 +0300

    work on section active

--- js/app.js
+++ js/app.js
@@ -7,45 +7,31 @@
  *  JS Version: ES2015/ES6
  *  JS Standard: ESlint
 */
-
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
 */
+// // build the navbar
 
-/*
- * Define Global Variables
-*
-*/
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// // build the nav
 // global variables
 const navbar = document.getElementById("navbar__list");
 const navItems = document.querySelectorAll("section");
-//forEach loop to creat li nav items
+
+//forEach loop to create li nav items
 const createNav = navItems.forEach(function(navItem, i){
   const newLi = document.createElement("li");
+
    //get the name by attribut data-nav
   let itemText = navItem.getAttribute('data-nav');
-  //creat htmlcontent with claas and link
+
+  //creat htmlcontent with id and link
   let itemHtml = `<a href=\"#${itemText}\" id=\"navLink_${[i]}\">${itemText}</a>`;
   newLi.innerHTML = itemHtml;
   newLi.className = "navListItem";
+
   //append new li to nav bar
   navbar.appendChild(newLi);
+
  // adding scroll to the links
   //choosing the <a> elements by id
   let navLinksItem = document.getElementById('navLink_'+ [i]);
@@ -54,15 +40,25 @@ const createNav = navItems.forEach(function(navItem, i){
     e.preventDefault();
     document.getElementById(itemText).scrollIntoView( {behavior: "smooth"}); 
     });
-
+    
 });
-createNav;
-// // Add class 'active' to section when near top of viewport
 
-// let listNavItems = navbar.getElementsByClassName("navListItem");
+// Add class 'active' to section when near top of viewport
 
-// for (let i = 0; i < listNavItems.length; i++) {
-//     listNavItems[i].addEventListener("click", function() {
+let sections = navbar.querySelectorAll("section");
+
+for (let i = 0; i < sections.length; i++) {
+  function isSecInView(sec){
+    const rect = sec.getBoundingClientRect();
+    return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)&&
+    rect.rigth <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+};
+    sections[i].addEventListener("click", function() {
 //     let current = document.getElementsByClassName("active");
 //     current[0].className = current[0].className.replace(" active", "");
 //     this.className += " active";
