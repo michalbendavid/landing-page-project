6493edcf6b010d4316183009a777e5aa7a4e64ea
michalbendavid <michalbendavid770@gmail.com>
2022-09-07 13:10:42 +0300

    debuging

--- js/app.js
+++ js/app.js
@@ -7,11 +7,8 @@
  *  JS Version: ES2015/ES6
  *  JS Standard: ESlint
 */
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-// // build the navbar
+
+// build the navbar
 
 // global variables
 const navbar = document.getElementById("navbar__list");
@@ -24,7 +21,7 @@ function createNav () {
  //get the name by attribut data-nav
   let itemText = navItem.getAttribute('data-nav');
  //creat htmlcontent with id and link
-  let itemHtml = `<a href=\"#${itemText}\" id=\"navLink_${[i]}\">${itemText}</a>`;
+  let itemHtml = `<a href=\"#${itemText}\" class=\"navLink\" id=\"navLink_${[i]}\">${itemText}</a>`;
   newLi.innerHTML = itemHtml;
   newLi.className = "navListItem";
 
@@ -34,43 +31,40 @@ function createNav () {
  // adding scroll to the links
   //choosing the <a> elements by id
   let navLinksItem = document.getElementById('navLink_'+ [i]);
-  navLinksItem.classList.remove("active");
+  
   //add an event listener to the links and scroll into veiw by id of the section
   navLinksItem.addEventListener("click", (e)=> {
+  
     e.preventDefault();
     document.getElementById(itemText).scrollIntoView( {behavior: "smooth"});
-    navLinksItem.classList.add("active");
+  
    });
 
   });
 } 
 createNav(); 
  
-
-    
+   
 // Add class 'active' to section when near top of viewport
 //creats list of sections
+let navLinks = document.querySelectorAll(".navLink");
 let sections = document.querySelectorAll("section");
 sections[0].classList.add("active");
-const viewH = document.documentElement.clientHeight;
+const viewH = screen.availHeight;
 document.addEventListener("scroll",function() {
   for (let i=0 ; i<sections.length ; i++){
       let rect = sections[i].getBoundingClientRect();
       let top = rect.top;
       let bottom = rect.bottom;
-      if (bottom >= 0 && bottom<=viewH && top>=0) {
-      sections[i].classList.add("active");
-      } else {sections[i].classList.remove("active");}
+      if (bottom >= 70 && top<=viewH) {
+        sections[i].classList.add("active");
+        navLinks[i].classList.add("active");
+      } else {sections[i].classList.remove("active");
+      navLinks[i].classList.remove("active");}
     };
   });
 
  
-//     sections[i].addEventListener("click", function() {
-// //     let current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//     });
-// };
 function formValid(){
   let myForm = document.getElementById("form");
   myForm.addEventListener("submit", (e)=>{
